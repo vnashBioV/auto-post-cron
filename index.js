58ef7a603cdf3164163baf44eeb5722d1b06c658
vnashBioV <tshilidzirambuda9@gmail.com>
2024-10-01 00:22:52 +0200

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   token: process.env.SANITY_TOKEN,
+  apiVersion: '2024-04-21',
   useCdn: false
 });
 
